87247e804ace0207d87bdfee5244fd2b12592db0
MEGA <MEGA@gmail.com>
2025-03-21 18:43:51 +0200

    Add project files.

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            Stack<int> stack = new Stack<int>();
+
+            Console.WriteLine("Въведете 10 числа:");
+
+           
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Число {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    queue.Enqueue(number);
+                }
+                else
+                {
+                    Console.WriteLine("Невалиден вход! Въведете цяло число.");
+                    i--; 
+                }
+            }
+
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+
+            Console.WriteLine("Числата в обратен ред:");
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+
+            Console.WriteLine(); 
+        }
+    }
+}
